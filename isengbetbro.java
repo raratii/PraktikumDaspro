91e0f9f34c724674a7ae99b30d7b8c9b3ab90afa
raratii <purnamaratih419@gmail.com>
2024-10-03 12:10:41 +0700

    commit pemilihan2

---
+++ isengbetbro.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/**
+ * isengbetbro
+ */
+public class isengbetbro {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int usia,tanggalLahir;
+        String bulanLahir;
+
+
+        System.out.println("Masukkan usia: ");
+        usia = sc.nextInt();
+        System.out.println("masukkan tanggal lahir: ");
+        tanggalLahir = sc.nextInt();
+        System.out.println("masukkan bulan lahir:");
+        bulanLahir = sc.nextLine();
+        
+
+
+    }
+}
